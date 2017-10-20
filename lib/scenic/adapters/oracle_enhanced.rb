88065abc8967bc8d75a9810234f0a5d1602eec49
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-10-20 15:16:48 -0400

    Basic Oracle adapter

---
+++ lib/scenic/adapters/oracle_enhanced.rb
@@ -0,0 +1,67 @@
+require_relative 'oracle_enhanced/views'
+
+module Scenic
+  module Adapters
+    # An adapter for managing Oracle views.
+    class OracleEnhanced
+      def initialize(connectable = ActiveRecord::Base)
+        @connectable = connectable
+      end
+
+      def views
+        Views.new(connection).all
+      end
+
+      def create_view(name, sql_definition)
+        execute "CREATE VIEW #{quote_table_name(name)} AS #{sql_definition};"
+      end
+
+      def update_view(name, sql_definition)
+        drop_view(name)
+        create_view(name, sql_definition)
+      end
+
+      def replace_view(name, sql_definition)
+        execute "CREATE OR REPLACE VIEW #{quote_table_name(name)} AS #{sql_definition};"
+      end
+
+      def drop_view(name)
+        execute "DROP VIEW #{quote_table_name(name)};"
+      end
+
+      def create_materialized_view(name, sql_definition)
+        execute "CREATE MATERIALIZED VIEW #{quote_table_name(name)} AS #{sql_definition};"
+      end
+
+      def update_materialized_view(name, sql_definition)
+        # IndexReapplication.new(connection: connection).on(name) do
+          drop_materialized_view(name)
+          create_materialized_view(name, sql_definition)
+        # end
+      end
+
+      def drop_materialized_view(name)
+        execute "DROP MATERIALIZED VIEW #{quote_table_name(name)};"
+      end
+
+      def refresh_materialized_view(name, concurrently: false, cascade: false)
+        # refresh_dependencies_for(name) if cascade
+
+        if concurrently
+          execute "REFRESH MATERIALIZED VIEW CONCURRENTLY #{quote_table_name(name)};"
+        else
+          execute "REFRESH MATERIALIZED VIEW #{quote_table_name(name)};"
+        end
+      end
+
+      private
+
+      attr_reader :connectable
+      delegate :execute, :quote_table_name, to: :connection
+
+      def connection
+        connectable.connection
+      end
+    end
+  end
+end
---
+++ lib/scenic/adapters/oracle_enhanced/views.rb
@@ -0,0 +1,59 @@
+module Scenic
+  module Adapters
+    class OracleEnhanced
+      class Views
+        def initialize(connection)
+          @connection = connection
+        end
+
+        def all
+          views_from_oracle.map(&method(:to_scenic_view))
+        end
+
+        private
+
+        attr_reader :connection
+
+        def views_from_oracle
+          # NOTE: Oracle cannot `UNION` on `LONG` fields,
+          # and the `all_mviews.query` field and `all_views.text` field
+          # are both `LONG` fields. Thus, we must use `UNION ALL`.
+          # Luckily, we are not here concerned about duplicates.
+          cursor = ActiveRecord::Base.connection.select_all(<<-SQL)
+            SELECT 'm'        AS KIND,
+                   mview_name AS VIEWNAME,
+                   owner      AS NAMESPACE,
+                   query      AS DEFINITION
+            FROM   all_mviews
+            UNION ALL
+            SELECT 'v'       AS KIND,
+                   view_name AS VIEWNAME,
+                   owner     AS NAMESPACE,
+                   text      AS DEFINITION
+            FROM   all_views
+          SQL
+        end
+
+        def to_scenic_view(result)
+          namespace, viewname = result[2], result[1]
+
+          if namespace != 'SYS'
+            namespaced_viewname = "#{oracle_identifier(namespace)}.#{oracle_identifier(viewname)}"
+          else
+            namespaced_viewname = oracle_identifier(viewname)
+          end
+
+          Scenic::View.new(
+            name: namespaced_viewname,
+            definition: result['DEFINITION'].strip,
+            materialized: result['KIND'] == 'm'
+          )
+        end
+
+        def oracle_identifier(name)
+          connection.quote_column_name_or_expression(name)
+        end
+      end
+    end
+  end
+end
